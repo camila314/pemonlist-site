ee190e9ef2087a17d24941483e940c3b25a49f10
KontrollFreek <williambush357@gmail.com>
2024-03-18 20:37:11 -0400

    full navbar

--- src/main.rs
+++ src/main.rs
@@ -114,6 +114,6 @@ async fn main() {
 
     // Set up 
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
