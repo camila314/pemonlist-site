0ef4f06cf2c8cee8f4b69d21734847a4b31b88a7
KontrollFreek <williambush357@gmail.com>
2024-04-13 00:47:43 -0400

    worthJumping

--- site/src/js/jump.js
+++ site/src/js/jump.js
@@ -1,8 +1,14 @@
+const jumpLogger = new Logger('Jump')
+
 const jumpButton = document.querySelector('.jump')
 const body = document.body
 
 ;(async function() {
     if (!jumpButton) return
+
+    const worthJumping = body.scrollHeight > window.innerHeight * 2
+
+    if (!worthJumping) return jumpLogger.log('Page too short to enable jump button')
 
     jumpButton.addEventListener('click', () => body.scrollTo({ top: 0, behavior: 'smooth' }))
 
